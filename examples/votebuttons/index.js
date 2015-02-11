cdc18db82784a25415fcca9f9ef3354e4cab27c6
Wang Qiu <winsonwq@gmail.com>
2015-02-11 15:25:02 +0800

    finish example on mobile app

--- examples/votebuttons/index.js
+++ examples/votebuttons/index.js
@@ -1,4 +1,6 @@
 var React = require('react');
 var App = require('./components/app.react');
+var FastClick = require('fastclick');
 
 React.render(<App />, document.getElementById('app'));
+FastClick(document.body);
