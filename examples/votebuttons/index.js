f07b164abb3582a0c14f3bf9e90fc8b1138915c0
Wang Qiu <winsonwq@gmail.com>
2015-12-15 20:53:23 +0800

    update replicate method rethink what is reactive react

--- examples/votebuttons/index.js
+++
@@ -1,6 +0,0 @@
-var React = require('react');
-var App = require('./components/app.react');
-var FastClick = require('fastclick');
-
-React.render(<App />, document.getElementById('app'));
-FastClick(document.body);
